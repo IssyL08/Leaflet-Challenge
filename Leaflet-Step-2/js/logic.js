5f10b7899c50ef40b0c3f469fce855a784e2d7ab
issyl <issyl@comcast.net>
2021-05-28 14:33:50 -0400

    05.28.2021

--- Leaflet-Step-2/js/logic.js
+++ Leaflet-Step-2/js/logic.js
@@ -1,15 +1,15 @@
 // Selectable backgrounds of our map - tile layers:
 // grayscale background.
 var graymap_background = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?" +
-  "access_token=pk.eyJ1IjoibWFudWVsYW1hY2hhZG8iLCJhIjoiY2ppczQ0NzBtMWNydTNrdDl6Z2JhdzZidSJ9.BFD3qzgAC2kMoEZirGaDjA");
+  "access_token={accessToken}");
 
 // satellite background.
 var satellitemap_background = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/256/{z}/{x}/{y}?" +
-  "access_token=pk.eyJ1IjoibWFudWVsYW1hY2hhZG8iLCJhIjoiY2ppczQ0NzBtMWNydTNrdDl6Z2JhdzZidSJ9.BFD3qzgAC2kMoEZirGaDjA");
+  "access_token={accessToken}");
 
 // outdoors background.
 var outdoors_background = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v9/tiles/256/{z}/{x}/{y}?" +
-  "access_token=pk.eyJ1IjoibWFudWVsYW1hY2hhZG8iLCJhIjoiY2ppczQ0NzBtMWNydTNrdDl6Z2JhdzZidSJ9.BFD3qzgAC2kMoEZirGaDjA");
+  "access_token={accessToken}");
 
 // map object to an array of layers we created.
 var map = L.map("mapid", {
